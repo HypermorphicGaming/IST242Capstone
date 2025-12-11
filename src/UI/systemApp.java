04bc580d5f7eb757d552a334010b49baf9478471
BigWill43 <whw150@icloud.com>
2025-12-10 23:01:09 -0500

    pushing some changes to view updates 👍

--- src/UI/systemApp.java
+++ src/UI/systemApp.java
@@ -2,6 +2,6 @@ package UI;
 
 public class systemApp {
     public static void main(String[] args){
-
+        new systemUI();
     }
 }
--- src/UI/systemUI.java
+++ src/UI/systemUI.java
@@ -1,4 +1,17 @@
 package UI;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
 public class systemUI {
+
+    public systemUI(){
+        JFrame frame = new JFrame("systemUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,600);
+
+        JPanel panel = new JPanel();
+    }
+
 }
